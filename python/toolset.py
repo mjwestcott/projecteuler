070ef924b5903b283ce84bcfdeaab97d75837030
Matt Westcott <m.westcott@gmail.com>
2016-01-10 12:57:01 +0000

    Add sieve of Eratosthenes from Alex Martelli

--- python/toolset.py
+++ python/toolset.py
@@ -20,6 +20,26 @@ def get_primes(start=2):
     """Yield prime numbers from start."""
     return filter(is_prime, count(start))
 
+# From Alex Martelli: http://stackoverflow.com/a/2212090
+def eratosthenes():
+    """Yields the sequence of prime numbers via the Sieve of Eratosthenes."""
+    D = {}  # map each composite integer to its first-found prime factor
+    for q in count(2):  # q gets 2, 3, 4, 5, ... ad infinitum
+        p = D.pop(q, None)
+        if p is None:
+            # q not a key in D, so q is prime, therefore, yield it
+            yield q
+            # mark q squared as not-prime (with q as first-found prime factor)
+            D[q*q] = q
+        else:
+            # let x <- smallest (N*p)+q which wasn't yet known to be composite
+            # we just learned x is composite, with p first-found prime factor,
+            # since p is the first-found prime factor of q -- find and mark it
+            x = p + q
+            while x in D:
+                x += p
+            D[x] = p
+
 #------------------------------------------------------------------------------
 # From the Python itertools docs
 
