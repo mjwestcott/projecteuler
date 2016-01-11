66bb4cefb144a5eda2a9168e3986476614b3fdc0
Matt Westcott <m.westcott@gmail.com>
2016-01-11 12:32:50 +0000

    Add problem53.py

---
+++ euler_python/problem53.py
@@ -0,0 +1,15 @@
+"""
+problem53.py
+
+How many, not necessarily distinct, values of nCr, for 1 ≤ n ≤ 100, are greater
+than one-million?
+"""
+from math import factorial
+
+def num_combinations(n, r):
+    return factorial(n) / (factorial(r) * factorial(n - r))
+
+def problem53():
+    return sum(1 for n in range(1, 100+1)
+                 for r in range(1, n+1)
+                 if num_combinations(n, r) > 1e6)
