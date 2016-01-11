0688aba059a1d2a79c7f09e9d2f69f262b923b08
Matt Westcott <m.westcott@gmail.com>
2016-01-11 12:24:41 +0000

    Remove unused prime generator and add SortedString

--- euler_go/src/euler/tools/tools.go
+++ euler_go/src/euler/tools/tools.go
@@ -5,24 +5,10 @@
 
 package tools
 
-import "math"
-
-// ReverseInts reverses a []int in place
-func ReverseInts(xs []int) {
-	for i, j := 0, len(xs)-1; i < j; i, j = i+1, j-1 {
-		xs[i], xs[j] = xs[j], xs[i]
-	}
-}
-
-// ReversedInts returns a new reversed []int
-func ReversedInts(xs []int) []int {
-	res := make([]int, len(xs))
-	copy(res, xs)
-	for i, j := 0, len(xs)-1; i < j; i, j = i+1, j-1 {
-		res[i], res[j] = res[j], res[i]
-	}
-	return res
-}
+import (
+	"math"
+	"sort"
+)
 
 // IsPrime checks whether n is prime
 func IsPrime(n int) bool {
@@ -40,17 +26,38 @@ func IsPrime(n int) bool {
 	return true
 }
 
-// GetPrimesFrom yields primes starting from the given value
-func GetPrimesFrom(start int) <-chan int {
-	ch := make(chan int)
-	go func() {
-		x := start
-		for {
-			if IsPrime(x) {
-				ch <- x
-			}
-			x++
-		}
-	}()
-	return ch
+//-----------------------------------------------------------------------------
+// Sorting
+
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool { return s[i] < s[j] }
+func (s sortRunes) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s sortRunes) Len() int           { return len(s) }
+
+// SortedString returns a new sorted string
+func SortedString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
+}
+
+//-----------------------------------------------------------------------------
+// Reversing
+
+// ReverseInts reverses a []int in place
+func ReverseInts(xs []int) {
+	for i, j := 0, len(xs)-1; i < j; i, j = i+1, j-1 {
+		xs[i], xs[j] = xs[j], xs[i]
+	}
+}
+
+// ReversedInts returns a new reversed []int
+func ReversedInts(xs []int) []int {
+	res := make([]int, len(xs))
+	copy(res, xs)
+	for i, j := 0, len(xs)-1; i < j; i, j = i+1, j-1 {
+		res[i], res[j] = res[j], res[i]
+	}
+	return res
 }
