e8943b9bfee307684158e92c2b2ca584b2c601ac
Matt Westcott <m.westcott@gmail.com>
2016-01-12 23:24:14 +0000

    Add problem55.go
    Also add ReversedString to tools.go

--- euler_go/src/euler/tools/tools.go
+++ euler_go/src/euler/tools/tools.go
@@ -81,3 +81,12 @@ func ReversedInts(xs []int) []int {
 	}
 	return res
 }
+
+// ReversedString returns a new reversed string
+func ReversedString(s string) string {
+	rs := []rune(s)
+	for i, j := 0, len(rs)-1; i < j; i, j = i+1, j-1 {
+		rs[i], rs[j] = rs[j], rs[i]
+	}
+	return string(rs)
+}
