905452a7a7e8939fce4a9e3559679bfd7882928e
Matt Westcott <m.westcott@gmail.com>
2016-01-18 23:24:05 +0000

    Use concurrent strategy in problem60.go

--- euler_go/src/euler/problem60.go
+++ euler_go/src/euler/problem60.go
@@ -15,33 +15,40 @@ import (
 	"euler/tools"
 	"fmt"
 	"strconv"
+	"sync"
 )
 
-// Used to memoize concatsToPrime.
-var cache = make(map[string]bool)
-
-// Translate the args to a string key to be used in the cache.
-func k(x, y int) string {
-	return strconv.Itoa(x) + strconv.Itoa(y)
+type cache struct {
+	sync.Mutex
+	m map[string]bool
 }
 
-func concatsToPrime(x, y int) bool {
-	if v, ok := cache[k(x, y)]; ok {
-		return v
+func newCache() *cache {
+	return &cache{sync.Mutex{}, make(map[string]bool)}
+}
+
+func (c *cache) concatsToPrime(x, y int) bool {
+	key := strconv.Itoa(x) + strconv.Itoa(y)
+	c.Lock()
+	defer c.Unlock()
+	// Try to find the answer in the cache.
+	if val, ok := c.m[key]; ok {
+		return val
 	}
+	// Otherwise find it manually and add to the cache.
 	xstr, ystr := strconv.Itoa(x), strconv.Itoa(y)
 	a, _ := strconv.Atoi(xstr + ystr)
 	b, _ := strconv.Atoi(ystr + xstr)
-	v := tools.IsPrime(a) && tools.IsPrime(b)
-	cache[k(x, y)] = v
-	return v
+	val := tools.IsPrime(a) && tools.IsPrime(b)
+	c.m[key] = val
+	return val
 }
 
-func allConcatToPrime(candidates []int) bool {
+func (c *cache) allConcatToPrime(candidates []int) bool {
 	for _, x := range candidates {
 		for _, y := range candidates {
 			if x != y && x < y {
-				if concatsToPrime(x, y) == false {
+				if c.concatsToPrime(x, y) == false {
 					return false
 				}
 			}
@@ -77,25 +84,35 @@ func (n node) sum() int {
 // Stack is a simple stack implementation.
 type stack []node
 
-// Pop an item off the stack. Panics if s is empty.
+// Pop a node off the stack. Panics if s is empty.
 func (s *stack) pop() node {
 	x := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
-	// Shrink the underlying array if the slice len <= 1/4 its size.
+
+	// Shrink the underlying array if the slice length <= 1/4 its capacity.
 	if len(*s) <= cap(*s)/4 {
 		*s = append([]node{}, *s...)
 	}
 	return x
 }
 
-// Push an item onto the stack.
+// Push a node onto the stack.
 func (s *stack) push(x node) {
 	*s = append(*s, x)
 }
 
+// We are going to use a concurrent depth-first search with a worker goroutine
+// pool of 4. Each goroutine will search for a solution from a different
+// starting prime. As soon as a solution is found, we return from the function.
+// Otherwise, we wait for all starting primes to be checked, and return an
+// error.
 func problem60() (int, error) {
 	// It's not clear how many primes to search through. Experimentation
-	// suggests 9000.
+	// suggests a limit of 9000 produces the correct answer: 26033. Note
+	// our algorithm does not guarantee the solution is the smallest
+	// possible, but as a matter of fact, it is. We could verify our
+	// answer by raising the limit to 26033, searching exhaustively, and
+	// observing that no smaller solutions are found.
 	limit := 9000
 	var primes []int
 	for i := 0; i < limit; i++ {
@@ -103,29 +120,69 @@ func problem60() (int, error) {
 			primes = append(primes, i)
 		}
 	}
-	tools.ReverseInts(primes) // We want to search smaller primes first from pop().
 
-	// Use depth-first search.
-	var frontier stack
-	for _, p := range primes {
-		frontier.push(node{p})
-	}
-	for len(frontier) != 0 {
-		n := frontier.pop()
-		if len(n) == 5 {
-			return n.sum(), nil
-		}
+	c := newCache()
+	ans := make(chan int)   // Used to send the answer.
+	done := make(chan bool) // Used to signal that all worker goroutines are done.
+	pchan := make(chan int) // Used to send worker goroutines a starting prime to search.
+	var wg sync.WaitGroup
+
+	go func() {
 		for _, p := range primes {
-			child := append(append(*new(node), n...), p)
-			if p > n.max() && allConcatToPrime(child) {
-				frontier.push(child)
+			pchan <- p
+		}
+		close(pchan)
+		wg.Wait()    // Wait for all workers to complete their search
+		done <- true // before sending completion signal.
+	}()
+
+	// Woker goroutine pool of 4.
+	for i := 0; i < 4; i++ {
+		go func() {
+			wg.Add(1)
+			for {
+				p, ok := <-pchan
+				if !ok {
+					wg.Done()
+					return
+				}
+
+				// Perform depth-first search starting at the given prime.
+				var frontier stack
+				frontier.push(node{p})
+
+				for len(frontier) != 0 {
+					n := frontier.pop()
+					if len(n) == 5 {
+						ans <- n.sum()
+					}
+					for _, prime := range primes {
+						child := append(append(*new(node), n...), prime)
+						if prime > n.max() && c.allConcatToPrime(child) {
+							frontier.push(child)
+						}
+					}
+				}
+
 			}
+		}()
+	}
+
+	for {
+		select {
+		case x := <-ans:
+			return x, nil
+		case <-done:
+			return -1, fmt.Errorf("No solution found, limit: %v", limit)
 		}
 	}
-	return -1, fmt.Errorf("problem60: no solution found, limit=%d", limit)
 }
 
 func main() {
-	ans, _ := problem60()
-	fmt.Println(ans)
+	ans, err := problem60()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(ans)
+	}
 }
