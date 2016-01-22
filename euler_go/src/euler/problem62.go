b1d4ef388032d3b8616b2299ed98122491031265
Matt Westcott <m.westcott@gmail.com>
2016-01-22 20:10:51 +0000

    Fix filename in comment in problem62.go

--- euler_go/src/euler/problem62.go
+++ euler_go/src/euler/problem62.go
@@ -1,4 +1,4 @@
-// problem62.py
+// problem62.go
 //
 // The cube, 41063625 (345**3), can be permuted to produce two other cubes:
 // 56623104 (384**3) and 66430125 (405**3). In fact, 41063625 is the smallest cube
