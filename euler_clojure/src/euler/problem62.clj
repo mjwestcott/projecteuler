db960743ab9f4671ab21d0f35ad756133cbaf8c9
Matt Westcott <m.westcott@gmail.com>
2016-04-16 11:20:50 +0100

    Add problem62.clj

---
+++ euler_clojure/src/euler/problem62.clj
@@ -0,0 +1,25 @@
+(ns euler.problem62
+  "https://projecteuler.net/problem=62
+
+  The cube, 41063625 (345**3), can be permuted to produce two other cubes:
+  56623104 (384**3) and 66430125 (405**3). In fact, 41063625 is the smallest
+  cube which has exactly three permutations of its digits which are also cube.
+  Find the smallest cube for which exactly five permutations of its digits are
+  cube."
+  (:require [clojure.string :as str]
+            [clojure.math.numeric-tower :as math]))
+
+(defn problem62 []
+  ;; Add i**3 to a map, the keys of which are an arbitrarily specified
+  ;; canonical permutation of its digits, the values of which are are lists of
+  ;; all cube numbers seen so far which are permutable to the key.
+  (loop [i 0
+         cubes {}]
+    (let [x (math/expt i 3)
+          canonical (str/join (sort (str x)))
+          seen (get cubes canonical (vector))
+          next (conj seen x)]
+      (if (= 5 (count next))
+        (apply min next)
+        (recur (inc i)
+               (assoc cubes canonical next))))))
