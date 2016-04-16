ae4b6438c2a6e615b7eb8a80f7bc7aafb8e7781f
Matt Westcott <m.westcott@gmail.com>
2016-04-16 23:15:05 +0100

    Use the digits func in toolset.clj

--- euler_clojure/src/euler/problem56.clj
+++ euler_clojure/src/euler/problem56.clj
@@ -3,14 +3,8 @@
 
   Considering natural numbers of the form, a**b, where a, b < 100,
   what is the maximum digital sum?"
-  (:require [clojure.math.numeric-tower :as math]))
-
-(defn digits [n]
-  (->> n                      ; 1234
-       (iterate #(quot % 10)) ; (1234, 123, 12, 1, 0, ...)
-       (take-while pos?)      ; (1234, 123, 12, 1)
-       (mapv #(mod % 10))     ; [4, 3, 2, 1]
-       rseq))                 ; (1 2 3 4)
+  (:require [clojure.math.numeric-tower :as math]
+            [euler.toolset :refer [digits]]))
 
 (defn digit-sum [n]
   (reduce + (digits n)))
