c00833fb28a84f14e56ee69ba940cc9d18c80d0e
Matt Westcott <m.westcott@gmail.com>
2016-04-16 15:18:41 +0100

    Add problem64.clj

---
+++ euler_clojure/src/euler/problem64.clj
@@ -0,0 +1,46 @@
+(ns euler.problem64
+  "https://projecteuler.net/problem=64
+
+  The first ten continued fraction representations of (irrational) square roots are:
+
+      sqrt(2)=[1;(2)]          period=1
+      sqrt(3)=[1;(1,2)]        period=2
+      sqrt(5)=[2;(4)]          period=1
+      sqrt(6)=[2;(2,4)]        period=2
+      sqrt(7)=[2;(1,1,1,4)]    period=4
+      sqrt(8)=[2;(1,4)]        period=2
+      sqrt(10)=[3;(6)]         period=1
+      sqrt(11)=[3;(3,6)]       period=2
+      sqrt(12)=[3;(2,6)]       period=2
+      sqrt(13)=[3;(1,1,1,1,6)] period=5
+
+  Exactly four continued fractions, for N <= 13, have an odd period. How many
+  continued fractions for N <= 10000 have an odd period?"
+  (:require [clojure.math.numeric-tower :as math]))
+
+(defn continued-fraction-sqrt [S]
+  ;; https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Continued_fraction_expansion
+  ;; Using variables S, m, d, a as in the URL above.
+  (let [root (math/sqrt S)]
+    (if (= root (math/floor root))
+      [root]
+      (loop [seen []
+             m 0
+             d 1
+             a (math/floor (math/sqrt S))]
+        (let [next-seen (conj seen {:m m, :d d, :a a})
+              next-m (- (* d a) m)
+              next-d (/ (- S (math/expt next-m 2)) d)
+              next-a (math/floor (/ (+ (math/floor (math/sqrt S)) next-m) next-d))]
+          ;; If the pattern repeats, return all the 'a' variables collected in order.
+          ;; Otherwise, continue the recursion.
+          (if (some #(= % {:m next-m, :d next-d, :a next-a}) next-seen)
+            (vec (for [x next-seen] (get x :a)))
+            (recur next-seen
+                   next-m
+                   next-d
+                   next-a)))))))
+
+(defn problem64 []
+  (let [cont-fracs (for [i (range 2 10001)] (continued-fraction-sqrt i))]
+    (count (filter #(odd? (count (rest %))) cont-fracs))))
